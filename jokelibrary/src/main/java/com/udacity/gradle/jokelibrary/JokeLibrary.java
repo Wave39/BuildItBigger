b661d57fd004caba5d65af4623b5f5208e6bd451
Brian Prescott <bp@wave39.com>
2015-11-03 23:13:32 -0500

    Added joke library

---
+++ jokelibrary/src/main/java/com/udacity/gradle/jokelibrary/JokeLibrary.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.jokelibrary;
+
+public class JokeLibrary {
+
+    public String getJoke() {
+        return "A Googler, a rabbi, and Jimi Hendrix walk into a bar...";
+    }
+
+}
