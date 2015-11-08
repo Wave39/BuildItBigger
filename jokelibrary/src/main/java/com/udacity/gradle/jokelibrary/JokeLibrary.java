3075fa527e6d249bdc39dc708590e84d7de9b42b
Brian Prescott <bp@wave39.com>
2015-11-08 15:15:04 -0500

    Changes to joke library

--- jokelibrary/src/main/java/com/udacity/gradle/jokelibrary/JokeLibrary.java
+++ jokelibrary/src/main/java/com/udacity/gradle/jokelibrary/JokeLibrary.java
@@ -1,9 +1,21 @@
 package com.udacity.gradle.jokelibrary;
 
+import java.util.Random;
+
 public class JokeLibrary {
 
+    private static String[] theJokes = {
+            "A Googler, a rabbi, and Jimi Hendrix walk into a bar...",
+            "There was a young lady from Nantucket...",
+            "There was a young lady from Venus...",
+            "Knock knock...",
+            "Did you hear the one about the human cannonball..."
+    };
+
     public static String getJoke() {
-        return "A Googler, a rabbi, and Jimi Hendrix walk into a bar...";
+        Random r = new Random();
+        int idx = r.nextInt(theJokes.length);
+        return theJokes[idx];
     }
 
 }
