812639fa53201aac605a5e9efa564c1288292ca9
Brian Prescott <bp@wave39.com>
2015-11-08 15:45:03 -0500

    Added async task class

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/GetJokeAsyncTask.java
@@ -0,0 +1,26 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * GetJokeAsyncTask
+ * Created by bp on 11/8/15.
+ */
+
+public class GetJokeAsyncTask extends AsyncTask<String, Void, String> {
+
+    public final String LOG_TAG = GetJokeAsyncTask.class.getSimpleName();
+
+    @Override
+    protected String doInBackground(String ... params) {
+
+        return "Async return value";
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Log.i(LOG_TAG, "onPostExecute result: " + result);
+    }
+}
