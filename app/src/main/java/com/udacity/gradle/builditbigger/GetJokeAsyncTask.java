c644c9b9e7aad8050b56a1417c335c2854b6c57c
Brian Prescott <bp@wave39.com>
2015-11-09 22:29:15 -0500

    Re-added backend and made other important tweaks

--- app/src/main/java/com/udacity/gradle/builditbigger/GetJokeAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/GetJokeAsyncTask.java
@@ -3,6 +3,8 @@ package com.udacity.gradle.builditbigger;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.example.bp.myapplication.backend.myApi.MyApi;
+
 /**
  * GetJokeAsyncTask
  * Created by bp on 11/8/15.
@@ -12,10 +14,12 @@ public class GetJokeAsyncTask extends AsyncTask<Void, Void, String> {
 
     public final String LOG_TAG = GetJokeAsyncTask.class.getSimpleName();
 
+    private static MyApi myApiService = null;
+
     @Override
     protected String doInBackground(Void... params) {
 
-        return "Async return value";
+        return "Async return value 2";
 
     }
 
